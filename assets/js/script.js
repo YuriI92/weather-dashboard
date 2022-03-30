6387f4550cd6ca5286ca33bafc69bca42c4f3c57
Yurippe <yurichikawa1992@gmail.com>
2022-03-30 12:11:46 -0400

    add weather icon and uvIndex scale to its data

--- assets/js/script.js
+++ assets/js/script.js
@@ -36,25 +36,42 @@ var getWeatherData = function(lat, lon) {
 }
 
 var today = moment().format("M/D/YYYY");
+var icon = "";
+var iconDesc =  "";
 var temp = "";
 var wind = "";
 var humid = "";
 var uvInd = "";
 
 var getCurrentWeather = function(data) {
+    icon = data.current.weather[0].icon;
+    iconDesc = data.current.weather[0].description;
     temp = data.current.temp;
     wind = data.current.wind_speed;
     humid = data.current.humidity;
     uvInd = data.current.uvi;
-    showCurrentWeather(temp, wind, humid, uvInd);
+    showCurrentWeather(icon, iconDesc, temp, wind, humid, uvInd);
 }
 
-var showCurrentWeather = function(temp, wind, humid, uvInd) {
+var showCurrentWeather = function(icon, iconDesc, temp, wind, humid, uvInd) {
     $("#city-name").text(cityName + " (" + today + ")");
+
+    $(".today-weather > div > img")
+        .attr("src", "http://openweathermap.org/img/wn/" + icon + ".png")
+        .attr("alt", iconDesc);
+
     $(".today-weather > p > .temp").text(temp);
     $(".today-weather > p > .wind").text(wind);
     $(".today-weather > p > .humid").text(humid);
     $(".today-weather > p > .uvInd").text(uvInd);
+
+    if (uvInd <= 2) {
+        $(".today-weather > p > .uvInd").addClass("bg-success text-white");
+    } else if (uvInd <= 5) {
+        $(".today-weather > p > .uvInd").addClass("bg-warning text-dark");
+    } else {
+        $(".today-weather > p > .uvInd").addClass("bg-danger text-white");
+    }
 }
 
 getGeoCoord();
